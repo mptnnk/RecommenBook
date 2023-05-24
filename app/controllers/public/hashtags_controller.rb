a796df53d5ece455953f603a34d19595ee8bd9c1
mptnnk <amzi1221@gmail.com>
2023-05-24 11:12:22 +0000

    [add]ハッシュタグ機能追加

--- app/controllers/public/hashtags_controller.rb
+++ app/controllers/public/hashtags_controller.rb
@@ -2,7 +2,10 @@ class Public::HashtagsController < ApplicationController
   def index
     @user = current_user
     @tag = Hashtag.find_by(name: params[:name])
-    @reviews = @tag.reviews
-    @tweets = @tag.tweets
+    # @reviews = @tag.reviews
+    # @tweets = @tag.tweets
+    @reviews = @tag.reviews.order(created_at: :desc)
+    @tweets = @tag.tweets.order(created_at: :desc)
+    @posts = (@reviews + @tweets).sort_by(&:created_at).reverse
   end
 end
