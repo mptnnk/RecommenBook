f8791b0c95e57de372974163cfcedaf7c2d70d5d
mptnnk <amzi1221@gmail.com>
2023-05-24 10:53:10 +0000

    [add]ハッシュタグ機能追加

--- app/helpers/public/tweets_helper.rb
+++ app/helpers/public/tweets_helper.rb
@@ -1,5 +1,9 @@
 module Public::TweetsHelper
-  def render_tweet_with_hashtags(tweet_content, length)
-    tweet_content.gsub(/[#＃][\w\p{Han}ぁ-ヶｦ-ﾟー]+/){|word| link_to word, "/hashtag/#{word.delete("#＃")}"}.html_safe
+  def render_tweet_with_hashtags(tweet_content, length = nil)
+    if length.nil?
+      tweet_content.gsub(/[#＃][\w\p{Han}ぁ-ヶｦ-ﾟー]+/){|word| link_to word, "/hashtag/#{word.delete("#＃")}"}.html_safe
+    else
+      tweet_content.truncate(length).gsub(/[#＃][\w\p{Han}ぁ-ヶｦ-ﾟー]+/){|word| link_to word, "/hashtag/#{word.delete("#＃")}"}.html_safe
+    end
   end
 end
