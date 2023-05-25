09f853109059139b7b48fdee18246bee65d49eb0
mptnnk <amzi1221@gmail.com>
2023-05-25 08:40:55 +0000

    [add]partial調整

--- app/controllers/public/review_comments_controller.rb
+++ app/controllers/public/review_comments_controller.rb
@@ -10,12 +10,19 @@ class Public::ReviewCommentsController < ApplicationController
   
   def index
     @user = User.find(params[:user_id])
+    @in_release_reviews = Review.where(user_id: @user.id, in_release: true).count
     @recommenbook = @user.favorite_books.find_by(recommenbook: true)
     if @recommenbook.present?
       @book = RakutenWebService::Books::Book.search(isbn: @recommenbook.isbn).first
     end
     @review_comments = ReviewComment.where(user_id: @user.id)
+    @review_comments.each do |comment|
+      @review = comment.review
+    end
     @tweet_comments = TweetComment.where(user_id: @user.id)
+    @tweet_comments.each do |comment|
+      @tweet = comment.tweet
+    end
     @comments = @review_comments + @tweet_comments
     @comments = @comments.sort_by { |comment| comment.created_at }.reverse
   end
