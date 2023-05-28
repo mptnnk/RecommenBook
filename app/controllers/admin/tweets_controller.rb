f342af69f8e7a21319d47b21dfb3c8454189cf93
mptnnk <amzi1221@gmail.com>
2023-05-28 13:53:59 +0000

    [add]adminツイート管理メソッド追加

--- app/controllers/admin/tweets_controller.rb
+++ app/controllers/admin/tweets_controller.rb
@@ -1,7 +1,17 @@
 class Admin::TweetsController < ApplicationController
+  before_action :authenticate_admin!
+  
   def index
+    @tweets = Tweet.all.order(created_at: :DESC).page(params[:page]).per(10)
   end
 
   def show
+    @tweet - Tweet.find(params[:id])
+  end
+  
+  def destroy
+    @tweet = Tweet.find(params[:id])
+    @tweet.destroy
+    redirect_to admin_tweets_path, alert: "ツイートを削除しました"
   end
 end
