f42865679100aca082da0a0a4301a276692bbbb7
mptnnk <amzi1221@gmail.com>
2023-05-28 08:39:09 +0000

    [add]admin review編集機能追加

--- app/controllers/admin/reviews_controller.rb
+++ app/controllers/admin/reviews_controller.rb
@@ -1,4 +1,19 @@
 class Admin::ReviewsController < ApplicationController
   def show
+    @review = Review.find(params[:id])
+    @book = RakutenWebService::Books::Book.search(isbn: @review.isbn).first
+    @book_favorites = FavoriteBook.where(isbn: @book.isbn)
   end
+  
+  def update
+    @review = Review.find(params[:id])
+    @review.update(review_params) ? (redirect_to admin_review_path(@review), notice: 'レビューのステータスを更新しました') : (render :show, alert: 'レビューのステータス更新に失敗しました')
+  end
+  
+  private
+  
+  def review_params
+    params.require(:review).permit(:in_release, :spoiler)
+  end
+    
 end
