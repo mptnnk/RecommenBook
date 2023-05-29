c66bf7b8c4656e6bd32faf8df823aa6c243a2422
mptnnk <amzi1221@gmail.com>
2023-05-29 04:40:03 +0000

    [add]adminツイート管理機能調整中

--- app/controllers/admin/tweets_controller.rb
+++ app/controllers/admin/tweets_controller.rb
@@ -2,11 +2,15 @@ class Admin::TweetsController < ApplicationController
   before_action :authenticate_admin!
   
   def index
-    @tweets = Tweet.all.order(created_at: :DESC).page(params[:page]).per(10)
+    if params[:user_id].present?
+      @tweets = Tweet.where(user_id: params[:user_id]).order(created_at: :DESC).page(params[:page]).per(10)
+    else
+      @tweets = Tweet.all.order(created_at: :DESC).page(params[:page]).per(10)
+    end
   end
 
   def show
-    @tweet - Tweet.find(params[:id])
+    @tweet = Tweet.find(params[:id])
   end
   
   def destroy
