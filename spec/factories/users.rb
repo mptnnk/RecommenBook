8b1e7833ea39b6b0f34fdc9eaf417aa6c98539b1
mptnnk <amzi1221@gmail.com>
2023-06-26 07:48:33 +0000

    [add]RSpecテスト作成

---
+++ spec/factories/users.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :user do
+    name { Faker::Lorem.characters(number: 6) }
+    email { Faker::Internet.email }
+    introduction { Faker::Lorem.characters(number: 10) }
+    password { 'password' }
+    password_confirmation { 'password' }
+    
+    after(:build) do |user|
+      user.profile_image.attach(io: File.open('app/assets/images/default-image.png'), filename: 'default-image.png', content_type: 'application/xlsx')
+    end
+    
+  end
+end
