02c19c409e8dd2f6d8f0e4bcc1e157397ac6099d
mptnnk <amzi1221@gmail.com>
2023-07-06 06:35:00 +0000

    [add]RSpecテスト system(user)作成

--- spec/system/public/users_spec.rb
+++ spec/system/public/users_spec.rb
@@ -35,32 +35,68 @@ RSpec.describe "ユーザー登録に関するテスト", type: :system do
   end
 
   describe "ユーザーログイン" do
+    let(:user) { create(:user) }
     before do
       visit root_path
     end
-
-    it "ログイン画面へ遷移する" do
-      click_on "ログイン", match: :first
-      expect(current_path).to eq "/users/sign_in"
-    end
-
-    context "ログインに成功する" do
-      let(:user) { create(:user) }
-      before do
+    
+    context "ログイン" do
+      it "ヘッダーからログイン画面に遷移できる" do
+        click_on "ログイン", match: :first
+        expect(current_path).to eq "/users/sign_in"
+      end
+      
+      it "ログイン画面からログインし、マイページに遷移する" do
         visit new_user_session_path
         fill_in "login_email_input", with: user.email
         fill_in "login_password_input", with: user.password
         click_button "ログイン"
-      end
-      it "正しくログインし、マイページに遷移する" do
         expect(current_path).to eq "/users/#{user.name}"
+      end
+    end
+    
+    # トップページのパーシャルでのログインテストを調べて実装する
+
+    context "ログイン失敗テスト" do
+      it "ログインに失敗し、ログイン画面にリダイレクトする" do
+        visit new_user_session_path
+        fill_in "login_email_input", with: ''
+        fill_in "login_password_input", with: ''
+        click_button "ログイン"
+        expect(current_path).to eq '/users/sign_in'
       end
     end
   end
 
   describe "ユーザーログアウト" do
+    let(:user) { create(:user) }
+    before do
+      visit new_user_session_path
+      fill_in "login_email_input", with: user.email
+      fill_in "login_password_input", with: user.password
+      click_button "ログイン"
+    end
+    it "ヘッダーからログアウトし、トップページに遷移する" do
+      click_on "ログアウト"
+      expect(current_path).to eq '/'
+    end
   end
 
   describe "プロフィール変更" do
+    let(:user) { create(:user) }
+    before do
+      visit new_user_session_path
+      fill_in "login_email_input", with: user.email
+      fill_in "login_password_input", with: user.password
+      click_button "ログイン"
+    end
+    it "プロフィール変更画面に遷移する" do
+      click_link "", href: users_information_edit_path
+      expect(current_path).to eq '/users/information/edit'
+    end
+    it "プロフィール変更し、マイページに遷移する" do
+      visit users_information_edit_path
+      
+    end
   end
 end
